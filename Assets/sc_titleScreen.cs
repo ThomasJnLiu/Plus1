b32a1e17619426ab2659467161ce79acc9ccecbb
Thomas <thomasjnliu@gmail.com>
2019-02-01 02:29:22 -0500

    added title screen

---
+++ Assets/sc_titleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sc_titleScreen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.LoadScene("Level 0");
+        }
+    }
+}
